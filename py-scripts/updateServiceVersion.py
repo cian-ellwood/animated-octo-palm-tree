8ed114203ae448a65a357b7584def2cfa4bcb571
Cian Ellwood <cian.ellwood@gmail.com>
2024-05-10 12:25:16 +0100

    Updated versioning script to update helm chart

--- py-scripts/updateServiceVersion.py
+++ py-scripts/updateServiceVersion.py
@@ -21,6 +21,8 @@ def set_new_service_version(calendar_version):
         data = json.load(version_data)
         current_version = data["version"]
         # If I take this in as an Integer I can increment easier down the line
+        # There would be conflicts for this versioning by incrementing manually
+        # Usually this wouldn't be ran until the CI stage and we would use the CI build number as minor version
         minor_version = int(data["current_minor_version"])
 
         # if the current year/week is not in the current version, use new year/week and reset minor version
@@ -49,14 +51,28 @@ def update_version_file(updated_version_json_file):
 
 def update_k8s_version(new_version):
     with open("k8s/apiLogger/Chart.yaml") as k8s_version:
-        k8_data = yaml.load(k8s_version, Loader=yaml.FullLoader)
-        k8_data["appVersion"] = new_version
+        updated_k8_data = yaml.load(k8s_version, Loader=yaml.FullLoader)
+        # Rather annoyingly regardless of combination of string when
+        # we format it the double quotes are dropped when we update the yaml
+
+        # Set chart version to be in line with application version
+        updated_k8_data["appVersion"] = new_version
+        updated_k8_data["version"] = new_version
+
+        return updated_k8_data
+
+
+def write_new_k8s_version(updated_k8_data):
+    with open("k8s/apiLogger/Chart.yaml", "w") as old_k8s_version:
+        yaml.dump(updated_k8_data, stream=old_k8s_version, default_flow_style=False, sort_keys=False)
 
 
 def main():
     calendar_version = format_current_week_and_year()
     updated_version_json_file, new_version = set_new_service_version(calendar_version)
     update_version_file(updated_version_json_file)
+    updated_k8s_data = update_k8s_version(new_version)
+    write_new_k8s_version(updated_k8s_data)
 
 
 if __name__ == "__main__":
